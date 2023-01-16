e16733213c92fcf41481a26da9d78b12ca813bfb
shafik720 <shafikrasel5@gmail.com>
2023-01-17 01:45:45 +0600

    setting up form

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,19 +2,25 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
+    const handleSaveButton = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const sector = e.target.sectors.value;
+        console.log(name, sector);
+    }
     return (
         <div className="home-div">
             <h2>Add Your Information Here : </h2>
             <hr />
             <div className="form-div">
-                <form action="" >
+                <form action=""  onSubmit={handleSaveButton}>
                     <div className="name-input-div">
                         <label htmlFor="name">Name :</label>
                         <input type="text" name="name" id="" />
                     </div>
                     <div className="sector-selection">
                         <label htmlFor="">Sector : </label>
-                        <select name="pets" size="4">
+                        <select name="sectors" size="4">
                             <optgroup label="4-legged pets">
                                 <option value="dog">Dog</option>
                                 <option value="cat">Cat</option>
@@ -28,7 +34,7 @@ const Home = () => {
                     </div>
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
-                        <label className="form-check-label fw-bold" for="flexCheckChecked">
+                        <label className="form-check-label fw-bold" htmlFor="flexCheckChecked">
                             Agree to terms
                         </label>
                     </div>
