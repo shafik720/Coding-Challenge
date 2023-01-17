593c3a4c1326398dfb7ee1b75f6d81961a7559c4
shafik720 <shafikrasel5@gmail.com>
2023-01-18 00:58:31 +0600

    active router styling added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
         <div className="header-div">
-            <Link to="/">Home</Link>
-            <Link to="/findUser">See User</Link>
+            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Home</NavLink>
+            <NavLink to="/findUser" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>See User</NavLink>
             {/* <Link to="/">Home</Link> */}
         </div>
     );
