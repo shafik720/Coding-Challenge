05d8762114b969134663cbd6872c85a86241e389
shafik720 <shafikrasel5@gmail.com>
2023-01-17 21:50:38 +0600

    working with selection option & its data

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 
 const Home = () => {
@@ -8,28 +8,44 @@ const Home = () => {
         const sector = e.target.sectors.value;
         console.log(name, sector);
     }
+
+    const [catagory, setCatagory] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => {
+                setCatagory(data);
+                setIsLoading(false);
+                console.log(catagory);
+            });
+    }, [isLoading]);
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
     return (
         <div className="home-div">
             <h2>Add Your Information Here : </h2>
             <hr />
             <div className="form-div">
-                <form action=""  onSubmit={handleSaveButton}>
+                <form action="" onSubmit={handleSaveButton}>
                     <div className="name-input-div">
                         <label htmlFor="name">Name :</label>
                         <input type="text" name="name" id="" />
                     </div>
                     <div className="sector-selection">
                         <label htmlFor="">Sector : </label>
-                        <select name="sectors" size="4">
-                            <optgroup label="4-legged pets">
-                                <option value="dog">Dog</option>
-                                <option value="cat">Cat</option>
-                            </optgroup>
-                            <optgroup label="Flying pets">
-                                <option value="parrot">Parrot</option>
-                                <option value="macaw">Macaw</option>
-                                <option value="albatross">Albatross</option>
-                            </optgroup>
+                        <select name="sectors" >
+    {/* ------------------ Sectors selection box will be shown here -------------- */}
+                            {
+                                catagory.map(index=><option 
+                                    value={index.deta[0]} 
+                                    disabled ={index.deta[0]==3 && 'true'}
+                                    className={index.deta[0]==3 && 'strong'}
+                                    >{index.deta[1]}</option>)
+                            }
+                            
                         </select>
                     </div>
                     <div className="form-check">
