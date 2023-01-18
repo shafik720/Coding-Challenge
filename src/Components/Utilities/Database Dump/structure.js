d62d696166ef4fc5bf1344d0217e2345b270c8ac
shafik720 <shafikrasel5@gmail.com>
2023-01-18 22:59:23 +0600

    changed some styles

---
+++ src/Components/Utilities/Database Dump/structure.js
@@ -0,0 +1,119 @@
+
+import { toast } from "react-toastify";
+
+
+// this function will add user to database
+const addToDb = (name, id, agree) => {
+    let dbUser = JSON.parse(sessionStorage.getItem('db-user') || '[]');
+
+    // add data to storage
+    const catagoryId = id;
+    const userName = name;
+    const isAgreed = agree;
+
+    const totalPacakge = { userName, catagoryId, isAgreed };
+
+    const existingUser = dbUser.find(user => user.userName === userName);
+    if (!existingUser) {
+        dbUser.push(totalPacakge);
+        sessionStorage.setItem('db-user', JSON.stringify(dbUser));
+        toast.success("User Created", {
+            position: "bottom-center",
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+        setTimeout(() => {
+            window.location.href = `/findSingleUser/${userName}`;
+            }, 500);
+    } else {
+        toast.error("User already Exist!", {
+            position: "bottom-center",
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+        return { existed: true };
+    }
+}
+
+//---------------------- edit a user
+
+const editToDb = (name, id, agree, previousUserName, previousCatagoryId) => {
+    let dbUser = JSON.parse(sessionStorage.getItem('db-user') || '[]');
+
+    // add data to storage
+    const catagoryId = id;
+    const userName = name;
+    const isAgreed = agree;
+
+    
+    const existingUserName = dbUser.find(user => user.userName === userName);
+    if (previousCatagoryId==catagoryId && existingUserName) {        
+        toast.error("You have not changed anything or the name already exists !", {
+            position: "bottom-center",
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    } else {
+        
+        for(let i=0; i<dbUser.length;i++){
+            // console.log(dbUser[i].userName==previousUserName);
+            if(dbUser[i].userName==previousUserName){
+                console.log(i);
+                dbUser.splice(i,1);
+            }
+        }
+        const totalPacakge = { userName, catagoryId, isAgreed };
+        dbUser.push(totalPacakge);
+        sessionStorage.setItem('db-user', JSON.stringify(dbUser));
+
+        toast.success("User Updated Successfully", {
+            position: "bottom-center",
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+        setTimeout(() => {
+        window.location.href = `/findSingleUser/${userName}`;
+        }, 400);
+    }
+}
+
+// this function will get user from database
+function getFromDb(name) {
+    let dbUser = JSON.parse(sessionStorage.getItem('db-user') || '[]');
+
+    const userName = name;
+    const existingUser = dbUser.find(user => user.userName === userName);
+    // if(!existingUser){
+    //     console.log('user not found');
+    // }else{
+    //     return dbUser;
+    // }
+    return existingUser;
+}
+
+
+export {
+    addToDb,
+    getFromDb,
+    editToDb
+}
