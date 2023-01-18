be7c5963ed630971bed286f695db348437c840c7
shafik720 <shafikrasel5@gmail.com>
2023-01-18 10:44:07 +0600

    routing too single user detail page done

--- src/Components/FindUser/FindSingleUser/FindSingleUser.js
+++ src/Components/FindUser/FindSingleUser/FindSingleUser.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { getFromDb } from '../../Utilities/fakeDb';
 
 const FindSingleUser = () => {
+    const{userName} = useParams();
+    
+    const user = getFromDb(userName);
+    console.log(user);
+
+    // const[user, setUser] = useState([]);
+    // const[loading, setLoading] = useState(true);
+    // useEffect(()=>{
+    //     const user = getFromDb(userName);
+    //     console.log(user);
+    //     if(user){
+    //         setLoading(false);
+    //         setUser(user)
+    //     }
+    // },[loading]);
+
+    // if(loading){
+    //     return <div>...loading</div>
+    // }
     return (
         <div>
-            <h2>User Details here</h2>
+            <h2>User Details here : {user.userName} </h2>
         </div>
     );
 };
