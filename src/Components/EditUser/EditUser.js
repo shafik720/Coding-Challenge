c89d3e3f2370993c757aca5f25be72c71da5fac9
shafik720 <shafikrasel5@gmail.com>
2023-01-18 16:06:04 +0600

    before facing data for sector catagory

--- src/Components/EditUser/EditUser.js
+++ src/Components/EditUser/EditUser.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import { getFromDb } from '../Utilities/fakeDb';
 
 const EditUser = () => {
+    const{userName} = useParams();
+
+    // getting user information from db
+    const user = getFromDb(userName);
     
     return (
         <div>
-            <h2>Edit User Here</h2>
+            <h2>Edit User Here : {userName} </h2>
         </div>
     );
 };
